5a753ce8ab20f14dcae96453d2484d58df60b17a
syrauff <syahrul1_s1sisfo@mahasiswa.ung.ac.id>
2025-08-21 22:17:23 +0800

    fix problem

--- resources/views/pages/admin/vision_mision/edit.blade.php
+++ resources/views/pages/admin/vision_mision/edit.blade.php
@@ -91,7 +91,7 @@
                 @endif
 
                 <form class="mx-auto max-w-full"
-                    action="{{ route('admin.visionmision.update', $visionMision) }}"
+                    action="{{ route('admin.visionmision.update', $visionmision) }}"
                     method="POST" enctype="multipart/form-data">
                     @method('PUT')
                     @csrf
