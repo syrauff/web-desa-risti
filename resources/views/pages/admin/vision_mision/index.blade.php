bc38ee870495d87d3cf29199662122e1b3a27783
syrauff <syahrul1_s1sisfo@mahasiswa.ung.ac.id>
2025-08-21 20:58:21 +0800

    change edit

--- resources/views/pages/admin/vision_mision/index.blade.php
+++ resources/views/pages/admin/vision_mision/index.blade.php
@@ -162,7 +162,7 @@
                                         <div class="flex items-center">
 
                                             <a
-                                                href="{{ route('admin.visionmision.edit', ['visionmision' => $vision_mision->slug]) }}">
+                                                href="{{ route('admin.visionmision.edit', $vision_mision) }}">
                                                 <svg class="size-6 text-yellow-500 dark:text-white" aria-hidden="true"
                                                     xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                                                     fill="none" viewBox="0 0 24 24">
